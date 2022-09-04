5ff336b29aad9d7e5bb1ed995d23b80cee581c9c
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-09-04 18:09:38 +0100

    refactor(dataprocessors): moved dataprocessor to commons folder

--- src/libs/InMemoryDB/DataProcessors/GraphicDataProcessor.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable class-methods-use-this */
-import { Color, ResourceGraphic } from '../Resources/Graphics/Graphic.type';
-
-export class GraphicDataProcessor {
-  process(data: ResourceGraphic): Color[] {
-    return data['idPkg:Graphic'].Color;
-  }
-}
