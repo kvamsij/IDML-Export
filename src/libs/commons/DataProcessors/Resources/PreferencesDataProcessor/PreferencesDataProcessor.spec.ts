8d4a9c695ad69863cb5b65537468cd5c05426d27
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-09-04 18:50:58 +0100

    refactor(preferencesdataprocessor): create document preferences data processor

---
+++ src/libs/commons/DataProcessors/Resources/PreferencesDataProcessor/PreferencesDataProcessor.spec.ts
@@ -0,0 +1,58 @@
+import { StylesMockData } from '@src/libs/commons/DataProcessors/Resources/StylesDataProcessor/Styles.data.mock';
+import { FastXMLParser } from '@src/libs/commons/FastXMLParser';
+import { FileParser } from '@src/libs/commons/FileParser/FileParser';
+import { FileParserInterface } from '@src/libs/commons/FileParser/FileParser.interface';
+import { ProcessorDataType } from '../interfaces/DataProcessorInterface';
+import { IDMLPreferences } from '../types/Preferences.type';
+import { PreferencesMockData } from './Preferences.data.mock';
+import { PreferenceDataProcessor } from './PreferencesDataProcessor';
+
+let parsedData: ProcessorDataType;
+let fileParser: FileParserInterface;
+
+beforeAll(async () => {
+  const xmlParser = new FastXMLParser().getInstance();
+  fileParser = new FileParser(xmlParser);
+  parsedData = await fileParser.parse(PreferencesMockData);
+});
+
+describe('Graphic Data Processor', () => {
+  describe('Initialization', () => {
+    it('should be able to call new() on PreferenceDataProcessor class', () => {
+      const preferences = new PreferenceDataProcessor();
+      expect(preferences).toBeTruthy();
+    });
+    it('should be able to call process()', async () => {
+      const preferences = new PreferenceDataProcessor();
+      const processSpy = jest.spyOn(preferences, 'process').mockImplementation();
+      preferences.process(parsedData);
+      expect(processSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('Errors', () => {
+    it('should not process data other than IDMLPreferences but throw an error', async () => {
+      const preferences = new PreferenceDataProcessor();
+      const processSpy = jest.spyOn(preferences, 'process');
+      const dataToProduceError: ProcessorDataType = await fileParser.parse(StylesMockData);
+
+      try {
+        preferences.process(dataToProduceError);
+      } catch (error) {
+        //
+      } finally {
+        expect(processSpy).toThrowError();
+      }
+    });
+  });
+
+  describe('Implementation', () => {
+    it('should return type {documentPreferences: DocumentPreference}', async () => {
+      const preferences = new PreferenceDataProcessor();
+      const expectedData = {
+        documentPreferences: (parsedData as IDMLPreferences)['idPkg:Preferences'].DocumentPreference,
+      };
+      const processedData = preferences.process(parsedData);
+      expect(processedData).toMatchObject(expectedData);
+    });
+  });
+});
---
+++ src/libs/commons/DataProcessors/Resources/PreferencesDataProcessor/PreferencesDataProcessor.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable class-methods-use-this */
+
+import { AbstractDataProcessor } from '../../AbstractorDataProcessor';
+import { ProcessorDataType, ProcessorReturnType } from '../interfaces/DataProcessorInterface';
+
+export class PreferenceDataProcessor extends AbstractDataProcessor {
+  process(data: ProcessorDataType): ProcessorReturnType {
+    if ('idPkg:Preferences' in data) return { documentPreferences: data['idPkg:Preferences'].DocumentPreference };
+    return super.process(data);
+  }
+}
