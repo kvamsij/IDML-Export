dd994ddaa587241112efaf42e7a2bffe5dc54cd7
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-09-05 12:01:09 +0100

    feat(story data processor): update ProcessorReturnType with Story[]

--- src/libs/commons/DataProcessors/Resources/interfaces/DataProcessorInterface.ts
+++ src/libs/commons/DataProcessors/Resources/interfaces/DataProcessorInterface.ts
@@ -1,16 +1,17 @@
+import { IDMLStory, Story } from '../../Stories/types/Story.type';
 import { Font, IDMLFonts } from '../types/Fonts.type';
 import { Color, ResourceGraphic } from '../types/Graphic.type';
 import { DocumentPreference, IDMLPreferences } from '../types/Preferences.type';
 import { CharacterStyle, IDMLStyles, ParagraphStyle } from '../types/Styles.type';
 
-export type ProcessorDataType = ResourceGraphic | IDMLPreferences | IDMLStyles | IDMLFonts | IDMLPreferences;
+export type ProcessorDataType = ResourceGraphic | IDMLPreferences | IDMLStyles | IDMLFonts | IDMLStory;
 
 export type ProcessorReturnType =
   | Color[]
   | { [key: string]: DocumentPreference }
   | Array<CharacterStyle | ParagraphStyle>
-  | Font[];
-
+  | Font[]
+  | Story[];
 export interface DataProcessorInterface {
   setNext(processor: DataProcessorInterface): DataProcessorInterface;
 
