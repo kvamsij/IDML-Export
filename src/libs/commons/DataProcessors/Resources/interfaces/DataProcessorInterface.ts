d4d0d3d1dc55dd75f2ec147c09c8ae6d4017aec5
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-09-05 13:00:09 +0100

    feat(data processor interface): update data processor interface with Spread Type

--- src/libs/commons/DataProcessors/Resources/interfaces/DataProcessorInterface.ts
+++ src/libs/commons/DataProcessors/Resources/interfaces/DataProcessorInterface.ts
@@ -1,17 +1,19 @@
+import { IDMLSpread, Spread } from '../../Spreads/types/Spreads.type';
 import { IDMLStory, Story } from '../../Stories/types/Story.type';
 import { Font, IDMLFonts } from '../types/Fonts.type';
 import { Color, ResourceGraphic } from '../types/Graphic.type';
 import { DocumentPreference, IDMLPreferences } from '../types/Preferences.type';
 import { CharacterStyle, IDMLStyles, ParagraphStyle } from '../types/Styles.type';
 
-export type ProcessorDataType = ResourceGraphic | IDMLPreferences | IDMLStyles | IDMLFonts | IDMLStory;
+export type ProcessorDataType = ResourceGraphic | IDMLPreferences | IDMLStyles | IDMLFonts | IDMLStory | IDMLSpread;
 
 export type ProcessorReturnType =
   | Color[]
   | { [key: string]: DocumentPreference }
   | Array<CharacterStyle | ParagraphStyle>
   | Font[]
-  | Story[];
+  | Story[]
+  | Spread[];
 export interface DataProcessorInterface {
   setNext(processor: DataProcessorInterface): DataProcessorInterface;
 
