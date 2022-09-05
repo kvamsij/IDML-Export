47e8d959f523528109f6a9ff44ef35f4f0e48265
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-09-05 16:50:47 +0100

    feat(dataprocessorinterface): update ProcessorReturnType as an object of data, datastore

--- src/libs/commons/DataProcessors/Resources/interfaces/DataProcessorInterface.ts
+++ src/libs/commons/DataProcessors/Resources/interfaces/DataProcessorInterface.ts
@@ -7,13 +7,16 @@ import { CharacterStyle, IDMLStyles, ParagraphStyle } from '../types/Styles.type
 
 export type ProcessorDataType = ResourceGraphic | IDMLPreferences | IDMLStyles | IDMLFonts | IDMLStory | IDMLSpread;
 
-export type ProcessorReturnType =
-  | Color[]
-  | { [key: string]: DocumentPreference }
-  | Array<CharacterStyle | ParagraphStyle>
-  | Font[]
-  | Story[]
-  | Spread[];
+export type ProcessorReturnType = {
+  dataStoreName: string;
+  processedData:
+    | Color[]
+    | { [key: string]: DocumentPreference }
+    | Array<CharacterStyle | ParagraphStyle>
+    | Font[]
+    | Story[]
+    | Spread[];
+};
 export interface DataProcessorInterface {
   setNext(processor: DataProcessorInterface): DataProcessorInterface;
 
