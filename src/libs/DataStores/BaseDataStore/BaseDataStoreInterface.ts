6fab560761fb4f70c8f41fff532d09a138e051d2
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-09-07 15:38:29 +0100

    refactor(basedatastore): delete basedatastore

--- src/libs/DataStores/BaseDataStore/BaseDataStoreInterface.ts
+++
@@ -1,5 +0,0 @@
-import { MapDataType } from '@src/libs/commons/DataLoaders/DataLoaderClientInterface';
-
-export interface BaseDataStoreInterface {
-  create(): Promise<Map<string, MapDataType>>;
-}
