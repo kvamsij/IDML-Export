f720acfddd370ac13a34942e878a35160a4243d3
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-09-09 15:07:15 +0100

    refactor(dataloaderclient): updates to class and interface
    Updated test suit and interface to exclude GetPathsReturnType from the types as it is not necessary

--- src/libs/commons/DataLoaders/DataLoaderClient.spec.ts
+++ src/libs/commons/DataLoaders/DataLoaderClient.spec.ts
@@ -1,4 +1,5 @@
 import { DataProcessorClient } from '../DataProcessors/DataProcessorClient';
+import { GetPathsReturnType, ProcessorReturnType } from '../DataProcessors/Resources/interfaces/DataProcessorInterface';
 import { DataLoaderClient } from './DataLoaderClient';
 import { preferences } from './mockData/Preferences.data.mock';
 import { styles } from './mockData/Styles.data.mock';
@@ -13,7 +14,10 @@ describe('DataLoaderClient', () => {
 
   it('should be able to call load()', async () => {
     const dataLoader = new DataLoaderClient();
-    const processedData = new DataProcessorClient().process(mockData.StylesMockData);
+    const processedData = new DataProcessorClient().process(mockData.StylesMockData) as Exclude<
+      ProcessorReturnType,
+      GetPathsReturnType
+    >;
     const loaderSpy = jest.spyOn(dataLoader, 'load').mockImplementation();
     dataLoader.load({ processedData, dataStore: new Map() });
     expect(loaderSpy).toBeCalledTimes(1);
@@ -22,7 +26,10 @@ describe('DataLoaderClient', () => {
   it('should have been called with provided data to load()', async () => {
     const dataLoader = new DataLoaderClient();
     const dataProcessor = new DataProcessorClient();
-    const processedData = dataProcessor.process(mockData.StylesMockData);
+    const processedData = dataProcessor.process(mockData.StylesMockData) as Exclude<
+      ProcessorReturnType,
+      GetPathsReturnType
+    >;
     const loadSpy = jest.spyOn(dataLoader, 'load').mockImplementation();
     dataLoader.load({ dataStore: new Map(), processedData });
     expect(loadSpy).toBeCalledWith({ dataStore: new Map(), processedData });
